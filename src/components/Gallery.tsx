ae7b01ed9caf4b7a4e778981a354fa842ce8c5e3
Naramatsu <joncyy_19@hotmail.com>
2024-06-20 08:37:06 -0500

    added Gallery interface

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -30,22 +30,21 @@ const Gallery = ({
   mediaList,
   fileSelected,
   setFileSelected,
-}: {
-  mediaList: FileModel[];
-  fileSelected: FileModel | null;
-  setFileSelected: any;
-}) => {
+}: GalleryInterface) => {
   return (
     <article className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 xs:grid-cols-1 gap-4 my-6 w-[80%] sm:w-[90%] m-auto relative">
       {mediaList.map((media, index) => (
         <section
-          className="w-full relative cursor-pointer"
+          className="file w-full relative cursor-pointer"
           key={index}
           onClick={() => setFileSelected(media)}
         >
           {mediaBuilder(media, index)}
           {fileSelected?.url === media?.url && (
-            <FaRegCheckCircle className="absolute left-2 bottom-2 text-white bg-emerald-700 rounded-full h-10 w-10" />
+            <FaRegCheckCircle
+              data-testid="svg-check"
+              className="absolute left-2 bottom-2 text-white bg-emerald-700 rounded-full h-10 w-10"
+            />
           )}
         </section>
       ))}
@@ -54,3 +53,9 @@ const Gallery = ({
 };
 
 export default React.memo(Gallery);
+
+export interface GalleryInterface {
+  mediaList: FileModel[] | [];
+  fileSelected: FileModel | null;
+  setFileSelected: any;
+}
